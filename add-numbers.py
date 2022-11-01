7b0fcf05b3042e6042dbb89865f5a450be87095b
raghav-a-gupta <raghav.a.gupta@in.ibm.com>
2022-11-01 17:55:51 +0530

    Reverted to 2 sum

--- add-numbers.py
+++ add-numbers.py
@@ -1,6 +1,11 @@
 x = input("Type a number: ")
 y = input("Type another number: ")
+<<<<<<< HEAD
 z = input("Type another number: ")
 sum = int(x) + int(y) + int(z)
+=======
+
+sum = int(y) + int(z)
+>>>>>>> parent of eb3dc0a (Replaced the z variable with x to rectify error)
 
 print("The sum is: ", sum)
