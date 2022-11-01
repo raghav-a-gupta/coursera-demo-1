1e9561467360141595651ba0cde5f0a35cc47469
raghav-a-gupta <raghav.a.gupta@in.ibm.com>
2022-11-01 17:43:15 +0530

    Functionality of adding in 3 numbers

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Coursera - Demo 1
 
 This is a sample README file for demonstration of basic git commands
+Adding a feature to dev branch for adding in 3 numbers.
--- add-numbers.py
+++ add-numbers.py
@@ -1,6 +1,6 @@
 x = input("Type a number: ")
 y = input("Type another number: ")
-
-sum = int(x) + int(y)
+z = input("Type another number: ")
+sum = int(x) + int(y) + int(z)
 
 print("The sum is: ", sum)
